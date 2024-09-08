8519b6894069342920d9ecb2ec1801dec786007f
Ricki Angel <angelricki@outlook.com>
2024-09-08 20:53:49 +0100

    Inc Update

--- config/configPath.js
+++ config/configPath.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 // Define the absolute path to the .env file
-const envPath = path.resolve('/Users/xeon2035/Library/CloudStorage/OneDrive-Personal/CONFIG_ENVS/node/env_reactlogin.txt');
+const envPath = path.resolve('/Users/xeon2035/Library/CloudStorage/OneDrive-Personal/CONFIG_ENVS/node/env_UnivDB-AWS.txt');
 
 // Export the path
 module.exports = {
